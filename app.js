69b3be404cb73c4f45cde5b58279da8dc4bb50b8
danielwang365 <danielwang1590@gmail.com>
2020-09-14 22:16:21 -0700

    Added Node.js Backend
    Node.js backend added, with React.js added as well. Access website through localhost:3000, and must have "node app.js" running in the terminal when wanting to access the site.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'))
+
+/*function sayHello(name){
+    console.log("Hello " + name);
+}
+
+sayHello("Daniel");*/
