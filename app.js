aacb504d6b931c251d697227302d4d6214dd0591
danielwang365 <danielwang1590@gmail.com>
2020-09-15 23:07:59 -0700

    Added /about subpage
    Added localhost:3000/about subpage

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const { response } = require('express');
 const express = require('express');
+const fs = require('fs');
+const { pathToFileURL } = require('url');
 const app = express();
 
+
+app.get('/about',function(req,res){
+    res.sendFile('/public/about_us.html',{ root: __dirname });
+});
+
 app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 /*function sayHello(name){
     console.log("Hello " + name);
