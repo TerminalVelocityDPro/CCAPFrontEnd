d8c6864670addc29a326a6339dee1d72f0732a34
DPro <35978720+TerminalVelocityDPro@users.noreply.github.com>
2020-09-15 23:20:42 -0700

    email notifications - need to examine security
    won't send to yahoo with ethereal email? Will need to figure that out. Probably just something to do with verification

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-const { response } = require('express');
+const nodemailer = require('nodemailer');
 const express = require('express');
-const fs = require('fs');
-const { pathToFileURL } = require('url');
 const app = express();
+const path = require('path');
+const router = express.Router();
 
-
-app.get('/about',function(req,res){
-    res.sendFile('/public/about_us.html',{ root: __dirname });
+const transporter = nodemailer.createTransport({
+	name: "ethereal.email",
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'elody81@ethereal.email',
+        pass: 'pvCK7C9PvyG4ePWFHm'
+    }
 });
 
-app.listen(3000, () => console.log('listening at 3000'));
+var mailOptions = {
+  from: 'elody81@ethereal.email',
+  to: 'elody81@ethereal.email',
+  subject: 'Sending Email using Node.js',
+  html: '<h1>Welcome</h1><p>That was easy!</p>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
 app.use(express.static('public'));
+
+
+app.use('/', router);
+app.listen(3000, () => console.log('listening at 3000'));
+
+
 
 /*function sayHello(name){
     console.log("Hello " + name);
