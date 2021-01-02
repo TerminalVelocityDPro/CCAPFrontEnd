4c8afbd244059727a94f287c87143e544a86b74a
Deepro Pasha <barnapa@yahoo.com>
2021-01-01 20:50:36 -0800

    wrote php backend code for iterating through the rows of the mysql table and displaying the encrypted firstname

--- backend.php
+++ backend.php
@@ -1,15 +1,23 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "ccapDB";
+
 include_once("db.php");
 
 
+try {
+  
+  $stmt = $conn->prepare("SELECT id, eFirstName, eLastName FROM myusers");
+  $stmt->execute();
 
+  // set the resulting array to associative
+  $result = $stmt->fetchAll();
+  foreach($result as $row){
+      echo $row['eFirstName'].'<br>';
+  }
 
-
-
-
+  
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
 
 ?>
