12806b806835e2dd029564afe22972818f5f3c63
Deepro Pasha <barnapa@yahoo.com>
2021-01-01 21:37:05 -0800

    added index.html and script.js

--- backend.php
+++ backend.php
@@ -2,13 +2,12 @@
 
 include_once("db.php");
 
-
 try {
   
   $stmt = $conn->prepare("SELECT id, eFirstName, eLastName FROM myusers");
   $stmt->execute();
 
-  // set the resulting array to associative
+
   $result = $stmt->fetchAll();
   foreach($result as $row){
       echo $row['eFirstName'].'<br>';
