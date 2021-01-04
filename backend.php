6757ae8b382b31f0a7c06d791e82ebe9877c8e3d
Deepro Pasha <barnapa@yahoo.com>
2021-01-03 17:31:22 -0800

    backend php able to access sql resources and put in array, sending array to js still working on it

--- backend.php
+++ backend.php
@@ -4,29 +4,75 @@ include_once("db.php");
 
 $allID = [];
 $allStress = [];
+$allStruggle = [];
+$allCovid = [];
+$allFamily = [];
+$allFriend = [];
+$allSchool = [];
+$allInterestProtect = [];
+$allHouseholdClean = [];
+$allStats = [];
+$allCSI = [];
+$allTrans = [];
+$allPeer = [];
+$allSocialHelp = [];
+$allTechSupport = [];
+$allTutor = [];
+$allDistance = [];
 
-//https://stackoverflow.com/questions/23740548/how-do-i-pass-variables-and-data-from-php-to-javascript
 
 
-try {
-  
+try {  
   $stmt = $conn->prepare("SELECT id, eStress, eStruggle, eCovid, eFamily, eFriend, eSchool, eInterestProtect, eHouseholdClean, eStats, eCSI, eTrans, ePeer, eSocialHelp, eTechSupport, eTutor, eDistance FROM myusers");
   $stmt->execute();
-
   $result = $stmt->fetchAll();
   foreach($result as $row){
     array_push($allID, $row['id']);
     array_push($allStress, $row['eStress']);
+    array_push($allStruggle, $row['eStruggle']);
+    array_push($allCovid, $row['eCovid']);
+    array_push($allFamily, $row['eFamily']);
+    array_push($allFriend, $row['eFriend']);
+    array_push($allSchool, $row['eSchool']);
+    array_push($allInterestProtect, $row['eInterestProtect']);
+    array_push($allHouseholdClean, $row['eHouseholdClean']);
+    array_push($allStats, $row['eStats']);
+    array_push($allCSI, $row['eCSI']);
+    array_push($allTrans, $row['eTrans']);
+    array_push($allPeer, $row['ePeer']);
+    array_push($allSocialHelp, $row['eSocialHelp']);
+    array_push($allTechSupport, $row['eTechSupport']);
+    array_push($allTutor, $row['eTutor']);
+    array_push($allDistance, $row['eDistance']);
+    $all = array(
+      "All ID" => $allID,
+      "All Stress" => $allStress,
+      "All Struggle" => $allStruggle,
+      "All COVID" => $allCovid,
+      "All Family" => $allFamily,
+      "All Friend" => $allFriend,
+      "All School" => $allSchool,
+      "All Interest Protect" => $allInterestProtect,
+      "All Household Clean" => $allHouseholdClean,
+      "ALL Stats" => $allStats, 
+      "All CSI" => $allCSI,
+      "All Trans" => $allTrans, 
+      "All Peer" => $allPeer,
+      "All Social Help" => $allSocialHelp,
+      "All Tech Support" => $allTechSupport,
+      "All Tutor" => $allTutor,
+      "All Distance" => $allDistance
+    );
       //echo json_encode($row['eFirstName'].'<br>');
   }
-
 } catch(PDOException $e) {
   //echo "Error: " . $e->getMessage();
 }
 
 $conn = null;
 
-//print_r($allStress)
+
+echo $all;
 //echo $allID;
 
 
