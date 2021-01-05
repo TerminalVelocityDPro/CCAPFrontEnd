adac4b0a0ba9fa33795fb85b9f435a8ea9277c5f
Jonah <jonahbedouch@gmail.com>
2021-01-04 16:42:22 -0800

    Rewrite backend fetch with async/await syntax in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
+import fetch from "node-fetch";
 import App from "./App.vue";
+
+// Get Data from the Backend
+const getData = async () => {
+  const data = await fetch("./php/backend.php", { method: "GET" });
+  const json = await data.json(); // Should eventually type this JSON
+
+  console.log(json);
+};
 
 createApp(App).mount("#app");
